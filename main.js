2d76a1acc22f9c5d56bedf731235efc9fa2b7d22
alexbakhu <aleqsandrekepler123@gmail.com>
2025-02-08 15:04:20 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const user1 = 'giorgi';
+const age1 = 20;
+
+const user2 = "mariami";
+const age2 = 18;
+
+const color1 = "black"
+const color2 = 'black'
+const color3 = 'black'
+
+const num1 = 7
+
+const num2 = -100
+
+const num3 = 15 * 3
+
+const num4 = 15+3
+
+const num5 = 15 - 3
+
+const num6 = 15 / 3
+
+const num7 = 123 * 654
+
+console.log(num7)
+
+console.log(num3)
+
+
+console.log(num4)
+
+
+console.log(num5)
+
+console.log(num6)
